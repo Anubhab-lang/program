34af28e8864f063b9c764a2f8bdcca0b12f2a020
Xingchenzhun <34294210+Xingchenzhun@users.noreply.github.com>
2018-04-02 10:49:38 +0800

    Family app

---
+++ Family/app/src/main/java/com/example/u/family/APP.java
@@ -0,0 +1,13 @@
+package com.example.u.family;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
